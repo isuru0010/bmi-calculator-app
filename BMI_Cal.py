5f68af643ad04f8c6a6d3ab99ee3b01198985043
Isuru Anuradha Wickramanayake <isuruanuradha260@gmail.com>
2026-05-26 12:38:44 +0530

    new version

--- BMI_Cal.py
+++ BMI_Cal.py
@@ -1,4 +1,7 @@
-import tkinter as tk
+import customtkinter as ctk
+
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("blue")
 
 def calculate_bmi():
     try:
@@ -22,33 +25,34 @@ def calculate_bmi():
         else:
             status="You are fat"
             color="red"
-        result_label.config(text="Your BMI is :" + str(bmi) + "Status: " + status, fg=color)
+        result_label.configure(text="Your BMI is :" + str(bmi) + "Status: " + status, text_color=color)
     except ValueError:
-        result_label.config(text+"Error")
+        result_label.configure(text="Error", text_color="red")
 
-window=tk.Tk()
-window.title("BMI Calculator")
+window=ctk.CTk()
+window.title("BMI Calculator Pro")
 
-window.iconbitmap("E:\work\python\BMI_Cal\pp_icon.ico")
+window.iconbitmap("E:\\work\\python\\BMI_Cal\\pp_icon.ico")
 window.geometry("600x350")
 
-title_label=tk.Label(window, text="BMI calculator", font=("Arial",16,"bold"))
-title_label.pack(pady=10)
 
-weight_label = tk.Label(window, text="Enter yout Weight( KG): ")
-weight_label.pack(pady=5)
-weight_entry = tk.Entry(window)
-weight_entry.pack()
+title_label=ctk.CTkLabel(window, text="BMI calculator", font=("Arial",24,"bold"))
+title_label.pack(pady=(30, 20))
 
-height_label = tk.Label(window, text="Enter yout Height( Meter, Ex: 1.75): ")
-height_label.pack(pady=5)
-height_entry = tk.Entry(window)
-height_entry.pack()
+#weight_label = ctk.CTkLabel(window, placeholder_text="Enter yout Weight(KG):", width=250, height=40, font=("Arial", 14))
+#weight_label.pack(10)
+weight_entry = ctk.CTkEntry(window, placeholder_text="Enter yout Weight(KG):", width=250, height=40, font=("Arial", 14))
+weight_entry.pack(pady=10)
 
-calc_button = tk.Button(window, text="Calculate", command = calculate_bmi, bg="lightblue", font=("Arial", 10, "bold"))
-calc_button.pack(pady=10)
+#height_label = ctk.CTkLabel(window, placeholder_text="Enter yout Height( Meter, Ex: 1.75): ", width=250, height=40, font=("Arial", 14))
+#height_label.pack(pady=10)
+height_entry = ctk.CTkEntry(window, placeholder_text="Enter yout Height( Meter, Ex: 1.75): ", width=250, height=40, font=("Arial", 14))
+height_entry.pack(pady=10)
 
-result_label= tk.Label(window, text="Result is here.", font=("Arial", 12))
+calc_button = ctk.CTkButton(window, text="Calculate", command = calculate_bmi, corner_radius=32, width=200, height=45, font=("Arial", 15, "bold"))
+calc_button.pack(pady=30)
+
+result_label = ctk.CTkLabel(window, text="Result is here.", font=("Arial", 16))
 result_label.pack(pady=10)
 
 window.mainloop()
